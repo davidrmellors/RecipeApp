add4f73fbb43f574af3cec323062681b3a16275c
davidrmellors <davidroymellors@gmail.com>
2023-06-07 12:20:12 +0200

    Warn user if recipe calories exceed 300
    User will now be warned if their recipe's calories exceed 300.

---
+++ RecipeAppTests/RecipeTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RecipeApp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipeApp.Tests
+{
+    [TestClass()]
+    public class RecipeTests
+    {
+        [TestMethod()]
+        public void RecipeCaloriesTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
